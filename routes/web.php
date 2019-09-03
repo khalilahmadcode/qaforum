999ed98f0b1273eb3389d9e5f2c685501015bed2
Khalil Nazari <khalil.nazari4@gmail.com>
2019-09-03 10:28:56 +0800

    Question view, controller and model is created and developed

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Route Questions
+Route::resource('questions', 'QuestionsController'); 
