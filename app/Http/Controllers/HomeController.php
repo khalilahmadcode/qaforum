b38446bb86fc6b437a7bc653d713b7aad3796f09
Khalil Nazari <khalil.nazari4@gmail.com>
2019-09-25 11:21:50 +0800

    Changes on home.blade.php and HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User; 
+use App\Question;
+use App\Answer; 
+use Illuminate\Support\Facades\DB;
+
 
 class HomeController extends Controller
 {
@@ -21,8 +26,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Question $question, Answer $answer)
     {
-        return view('home');
+        // $qs = $question->user; 
+        // echo 'test'; 
+        // print_r($qs);
+        // die; 
+        $user = auth()->user();
+        $questions = DB::table('questions')->where('user_id', $user->id)->get();
+        return view('home')->with([
+            'user'=>$user, 
+            'questions'=>$questions
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,37 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+                    Dashboard
+                    <span class="float-right">
+                        Welcome {{ $user->name }}
+                    </span>
+                </div>
+                
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <a class="btn btn-outline-primary btn-lg mb-2" href="{{ url('questions') }}">All Questions</a>
+                    <div class="list-group">
+                        <h3 class="list-group-item active">Questions List</h3>
 
-                    You are logged in!
+                        @foreach ($questions as $question)
+                            <div class="row mb-1 mt-1">
+                                <div class="col-10">
+                                    <h4 class="list-group-item list-group-item-action">
+                                        <a href="{{ url('questions/'.$question->slug) }}">{{ $question->title }}</a>
+                                    </h4>
+                                </div>
 
-                    <a class="float-right" href="{{ url('questions') }}">Questions</a>
+                                <div class="col-2 item-align-right">
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-success">Edit</a>
+                                    <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        @endforeach    
+                    </div>
                 </div>
             </div>
         </div>
